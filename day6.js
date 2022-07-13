8256c44b973e4fc0089f95452f3db451ac1e0c18
chaim-007 <mailchaim007@gmail.com>
2022-07-13 16:55:45 +0530

    pallindrom

---
+++ day6.js
@@ -0,0 +1,32 @@
+console.log('connected')
+
+// let number=5;
+// if(number == 5){
+//     console.log('number is 5')
+// } else if(number >= 5){
+//     console.log('number is grater than five ')
+// }
+
+// function enterNumber(a,b){
+//     console.log('Your sum of numbers  is' + a + b)
+// }
+
+// return 'You can enter more numbers'
+
+// let number = enterNumber()
+// console.log(number)
+
+// for(let i=5; i<51; i=i+5){
+//     console.log(i)
+// }
+
+// for(i=1; i<2;i){
+//     console.log(i)
+// }
+
+// let i=0
+// while(i < 10){
+//     console.log(i)
+//     i++
+// }
+
---
+++ pallindrom.js
@@ -0,0 +1,16 @@
+console.log('connected')
+
+function Pal(wo){
+    const w = wo.split('');
+    const rev= w.reverse();
+    const  w2= rev.join('');
+    if(wo == w2) {
+        console.log('It is a pallindrome');
+    }
+    else {
+        console.log('It is not a pallindrome');
+    }
+}
+const wo = prompt('Enter a word: ');
+
+Pal(wo);
